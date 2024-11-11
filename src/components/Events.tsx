d5f2e049b5db0381f6561fdbbeffcf36860da564
Daniel Navarro <daniel.navarro@newtech.consulting>
2024-11-11 20:20:40 +0000

    Events updated

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,18 +3,28 @@ import { motion } from 'framer-motion';
 const Events = () => {
   const events = [
     {
+      date: "NOV 10",
+      venue: "Rugby Lounge Club",
+      location: "Coimbra, PT",
+      time: "02:00 - 03:00",
+      map: "https://maps.app.goo.gl/TE1thqcZha74ogDk6",
+      finished: true
+    },
+    {
       date: "NOV 15",
       venue: "Club Inifinity",
       location: "Leiria, PT",
       time: "22:00 - 04:00",
-      map: "https://maps.app.goo.gl/9hA5JxnXdzjD9Lp16"
+      map: "https://maps.app.goo.gl/9hA5JxnXdzjD9Lp16",
+      finished: false
     },
     {
-      date: "NOV 21",
+      date: "DIC 06",
       venue: "Rugby Lounge Club",
       location: "Coimbra, PT",
-      time: "20:00 - 04:00",
-      map: "https://maps.app.goo.gl/TE1thqcZha74ogDk6"
+      time: "01:00 - 04:00",
+      map: "https://maps.app.goo.gl/TE1thqcZha74ogDk6",
+      finished: false
     }
   ];
 
@@ -44,10 +54,10 @@ const Events = () => {
             >
               <div className="flex items-center space-x-6 mb-4 md:mb-0">
                 <div className="text-center md:text-left">
-                  <h3 className="text-2xl font-bold gradient-text font-orbitron">{event.date}</h3>
+                  <h3 className={`text-2xl font-bold ${event.finished ? "line-through text-gray-300" : "gradient-text"} font-orbitron`}>{event.date}</h3>
                 </div>
                 <div>
-                  <h4 className="text-xl font-semibold hover:text-blue-400 transition-colors">
+                  <h4 className={`text-xl font-semibold ${event.finished ? "line-through" : "hover:text-blue-400"} transition-colors`}>
                     <a target="_blank" href={event.map} className='hover:cursor-pointer hover:text-blue-400 transition-colors'>
                     {event.venue}
                     </a>
@@ -57,11 +67,11 @@ const Events = () => {
                 </div>
               </div>
               <motion.button
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                className="bg-gradient-to-r from-lime-600 to-green-600 text-white px-6 py-2 rounded-full hover:from-lime-700 hover:to-green-700 transition duration-300"
+                whileHover={!event.finished ? { scale: 1.05 } : undefined}
+                whileTap={!event.finished ? { scale: 0.95 } : undefined}
+                className={`bg-gradient-to-r px-6 py-2 rounded-full transition duration-300 ${event.finished ? "from-gray-700 to-gray-500 text-gray-400" : "from-lime-600 to-green-600 text-white hover:from-lime-700 hover:to-green-700"}`}
               >
-                <a className='w-full h-full' href='tel:+351912553887'>Comprar Tickets </a>
+                <a className={`w-full h-full ${event.finished && 'line-through'}`} href='tel:+351912553887'>Comprar Tickets </a>
               </motion.button>
             </motion.div>
           ))}
